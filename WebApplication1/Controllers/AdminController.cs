d920ed5f040d32cbb0b2e2d757aca9eee9e8fab6
kyzya30 <kyzya30@inbox.ru>
2015-09-06 16:27:32 +0300

    1

---
+++ WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Statistics()
+        {
+            return View();
+        }
+        public ActionResult Category()
+        {
+            return View();
+        }
+        public ActionResult Dishes()
+        {
+            return View();
+        }
+        public ActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
